9cc062ed70e0138cde925c4f5b4f96c6fe290022
Jian Li <jianli8@gmail.com>
2018-04-11 22:13:17 -0500

    Registration and login REST API updated for error handling

--- server/api/movie/index.js
+++ server/api/movie/index.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Keyhole Software LLC
+Copyright 2018 Keyhole Software LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
